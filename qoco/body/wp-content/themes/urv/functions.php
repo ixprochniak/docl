856c0c94676648eb8fd8debdd92d8a9627202b8d
Ix. Próchniak <ix.prochniak@192.168.1.31>
2025-10-05 05:35:01 +0200

    Commit

--- qoco/body/wp-content/themes/urv/functions.php
+++ qoco/body/wp-content/themes/urv/functions.php
@@ -15,6 +15,18 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'urv' );
 
     wp_add_inline_style( 'urv',
+    '@font-face' .
+    "{ font-family: Lr; font-style: italic; font-weight: 400 700; font-display: swap; src: url('/upload/i05TX.woff2') format('woff2'); unicode-range: U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF; }" .
+
+    '@font-face' .
+    "{ font-family: Lr; font-style: italic; font-weight: 400 700; font-display: swap; src: url('/upload/COm0v.woff2') format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }" .
+
+    '@font-face' .
+    "{ font-family: Rc; font-style: normal; font-weight: 300 900; font-display: swap; src: url('/upload/OiNLh.woff2') format('woff2'); unicode-range: U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF; }" .
+
+    '@font-face' .
+    "{ font-family: Rc; font-style: normal; font-weight: 300 900; font-display: swap; src: url('/upload/cJ0iC.woff2') format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }" .
+
     '*,*::before,*::after' .
     '{ box-sizing: border-box; }' .
 
@@ -31,7 +43,7 @@ add_action( 'wp_enqueue_scripts', function() {
     'figure,figcaption,footer,header,hgroup,' .
     'menu,nav,output,ruby,section,summary,' .
     'time,mark,audio,video' .
-    '{ margin: 0; padding: 0; border: 0; }' .
+    '{ margin: 0; padding: 0; border: 0; font: inherit; }' .
 
     'article,aside,details,figcaption,figure,' .
     'footer,header,hgroup,menu,nav,section' .
@@ -41,7 +53,14 @@ add_action( 'wp_enqueue_scripts', function() {
     '{ list-style-type: none; }' .
 
     'body' .
-    '{ -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; }'
+    '{ -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; }' .
+
+    '.elementor-widget-heading' .
+    "{ font-family: 'Lr'; font-weighy: 390; }" .
+
+    '.elementor-widget-text-editor,' .
+    '.elementor-widget-button' .
+    "{ font-family: 'Rc'; font-weight: 355; }"
     );
 }, 0 );
 
